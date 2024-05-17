92c74bcb0f6546615aace78373de96501edaf985
cylaubq <cylaubq@connect.ust.hk>
2024-05-17 12:58:54 +0200

    Add inheritance. LeftEye & RightEye inheriting Eye.

---
+++ LeftEye.java
@@ -0,0 +1,10 @@
+package bull;
+
+import java.awt.Color;
+
+public class LeftEye extends Eye {
+
+	public LeftEye(Color color, int width, int height) {
+		super(color, width, height);
+	}
+}
---
+++ RightEye.java
@@ -0,0 +1,9 @@
+package bull;
+
+import java.awt.Color;
+
+public class RightEye extends Eye {
+	public RightEye(Color color, int width, int height) {
+		super(color, width, height);
+	}
+}
