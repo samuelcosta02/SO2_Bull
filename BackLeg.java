3b28da4dceb3a7eaea9696c32d17cebf6a7d1b8c
cylaubq <cylaubq@connect.ust.hk>
2024-05-17 12:59:46 +0200

    Minor adjustments for FrontLeg & BackLeg

--- BackLeg.java
+++ BackLeg.java
@@ -7,9 +7,4 @@ public class BackLeg extends Leg {
     public BackLeg(Color color, int width, int height) {
         super(color, width, height);
     }
-    
-    public void drawAt(int x, int y) {
-        Drawing.pen().setColor(color);
-        Drawing.pen().fillRect(x, y, width, height);
-    }
 }
--- FrontLeg.java
+++ FrontLeg.java
@@ -7,9 +7,4 @@ public class FrontLeg extends Leg {
     public FrontLeg(Color color, int width, int height) {
         super(color, width, height);
     }
-    
-    public void drawAt(int x, int y) {
-        Drawing.pen().setColor(color);
-        Drawing.pen().fillRect(x, y, width, height);
-    }
 }
